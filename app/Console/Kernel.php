621522ae883625df690bee3514a58e455791c8a9
hossein <h.faghihi15@gmail.com>
2016-12-01 01:28:52 +0330

    Add Cron Job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\User;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,8 +25,18 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            $users=User::all();
+            foreach ($users as $user){
+                foreach ($user->pack_take as $instace){
+                    $end=$instace->pivot->end;
+                    $today=date('Y-m-d H:i:s');
+                    if($today>$end)
+                        $user->pack_take()->detach($instace['id']);
+
+                }
+            }
+        })->daily();
     }
 
     /**
