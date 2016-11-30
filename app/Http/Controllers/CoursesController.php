333f77c2636da8b5014117a36108ed58de3469ef
hossein <h.faghihi15@gmail.com>
2016-11-30 13:57:17 +0330

    Course Controller Buy add

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -244,6 +244,21 @@ class CoursesController extends Controller
         return $this->ShowReviews($course);
     }
 
+    public function AddCourse(Course $course,$payment,$discount)
+    {
+        $user=\Auth::user();
+        try {
+            $user->courses_take()->attach($course->id,['paid'=>$payment,'discunt_used'=>$discount]);
+        }
+        catch ( \Illuminate\Database\QueryException $e){
+
+            return 0;
+        }
+
+        return 1;
+
+    }
+
     /**
      *
      * Remove Course from storage.
