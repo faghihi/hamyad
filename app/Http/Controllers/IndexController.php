c7fa9c867a8d3c91913bc63f80d3c818d1d1d225
hossein <h.faghihi15@gmail.com>
2016-11-30 15:29:17 +0330

    Index Controller Course Completed

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -26,6 +26,39 @@ class IndexController extends Controller
                 break;
             }
         }
+
+        foreach ($Data['Courses'] as $course ){
+            $courses = Course::all();
+            foreach ($courses as $course){
+                $course['Teachers']="";
+                $counter=0;
+                foreach ($course->teachers as $teacher){
+                    if($counter)
+                        $course['Teachers']=$course['Teachers'].",".$teacher->name;
+                    else
+                        $course['Teachers']=$teacher->name;
+                    $counter++;
+                }
+                $rate_count=0;
+                $rate_value=0;
+                foreach ($course->rates as $rate){
+                    $rate_count++;
+                    $rate_value +=$rate->pivot->rate;
+                }
+                $count=0;
+                $time=0;
+                foreach ($course->section as $section){
+                    $count++;
+                    $time+=$section->time;
+                }
+                $course['sections_time']=$time;
+                $course['sections_count']=$count;
+                $course['rates_value']=$rate_value;
+                $course['rates_count']=$rate_count;
+                $counter=$course->category->name;
+
+            }
+        }
 //        $TVs=TV::all();
 //        $Data['TV']=$TVs;
         $Categories=Category::all();
