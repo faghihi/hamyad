f885a71ea583193fdf4cd96e591bace76a204e64
hossein <h.faghihi15@gmail.com>
2016-11-30 17:12:54 +0330

    Validting try number 3

--- app/Contact.php
+++ app/Contact.php
@@ -7,10 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     protected $table="contacts";
-    protected $rules = array(
-        'name' => 'Required|Min:3|Max:80',
-        'email'     => 'Required|Between:3,64|Email',
-        'message' => 'Required|Min:10'
-    );
 
 }
--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Contact;
 use App\Subscribe;
+use Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 
@@ -26,20 +27,29 @@ class SocialController extends Controller
 
     public function StoreContact($input)
     {
-        if(isset($input['Name'])&&isset($input['Email'])&&isset($input['Message'])){
-            $message=new Contact();
-            $message->name=$input['Name'];
-            $message->email=$input['Email'];
-            if(isset($input['Subject']))
-                $message->subject=$input['Subject'];
-            $message->message=$input['Message'];
-            try{
-                $message->save();
+        $rules = array(
+            'name' => 'Required|Min:3|Max:80',
+            'email'     => 'Required|Between:3,64|Email',
+            'message' => 'Required|Min:10'
+        );
+
+        $validator = Validator::make($input, $rules);
+        if (! $validator->fails()) {
+            if (isset($input['Name']) && isset($input['Email']) && isset($input['Message'])) {
+                $message = new Contact();
+                $message->name = $input['Name'];
+                $message->email = $input['Email'];
+                if (isset($input['Subject']))
+                    $message->subject = $input['Subject'];
+                $message->message = $input['Message'];
+                try{
+                    $message->save();
+                }
+                catch ( \Illuminate\Database\QueryException $e){
+                    return 0;
+                }
+                return 1;
             }
-            catch ( \Illuminate\Database\QueryException $e){
-                return 0;
-            }
-            return 1;
         }
         return 0;
     }
