85e453af1e5676081262e7443dbe7a533832f49b
hossein <h.faghihi15@gmail.com>
2016-11-30 19:42:13 +0330

    Search For Teachers

--- app/Http/Controllers/TeachersController.php
+++ app/Http/Controllers/TeachersController.php
@@ -30,6 +30,27 @@ class TeachersController extends Controller
         return $this->RetrieveData();
     }
 
+    public function Search()
+    {
+        $input = Input::all();
+        if(isset($input['name'])){
+            return redirect('/');
+        }
+        $teachers=Teacher::where('name','like','%'.$input['name'].'%');
+        foreach ($teachers as $teacher){
+            $teacher['Course_count']=count($teacher->courses);
+            $rate_count=0;
+            $rate_value=0;
+            foreach ($teacher->rates as $rate){
+                $rate_count++;
+                $rate_value +=$rate->pivot->rate;
+            }
+            $teacher['rates_value']=$rate_value;
+            $teacher['rates_count']=$rate_count;
+        }
+        return $teachers;
+    }
+
     public function ShowSpecific(Teacher $teacher)
     {
         $counter=$teacher->courses;
