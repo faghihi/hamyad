6494fa16ee609b5e94a9fb3832c2321a08a1cc58
RshNk <rk.mirzaee.m@gmail.com>
2016-12-17 22:46:46 +0330

    28/9/95 view changed detail

--- resources/views/profile/user-profile.blade.php
+++ resources/views/profile/user-profile.blade.php
@@ -584,7 +584,79 @@
 <!-- end Back To Top -->
 
 <div id="packModalProfile" class="modal fade login-box-wrapper" data-width="500" data-backdrop="static" data-keyboard="false" tabindex="-1" style="display: none;">
-	@include('courses.package-purchase-modal')
+	<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+		<h4 class="modal-title text-center">لیست دروس بسته</h4>
+	</div>
+
+	<div class="modal-body">
+		<div class="row gap-20">
+
+			<div class="related-course mb-30">
+
+				<ul class="related-course-item mb-20">
+					<li class="clearfix">
+						<a href="#">
+							<div class="image">
+								<img src="images/course/course-item-sm-01.jpg" alt="Related Course" />
+							</div>
+							<div class="content">
+								<h6>Become a Certified Hadoop Developer</h6>
+								<div class="rating-wrapper">
+									<div class="rating-item">
+										<input type="hidden" class="rating" data-filled="fa fa-star" data-empty="fa fa-star-o" data-fractions="2" data-readonly value="3.5"/>
+									</div>
+									<span>(7 review)</span>
+								</div>
+								<span class="price">$199.<small>98</small></span>
+							</div>
+						</a>
+					</li>
+					<li class="clearfix">
+						<a href="#">
+							<div class="image">
+								<img src="images/course/course-item-sm-02.jpg" alt="Related Course" />
+							</div>
+							<div class="content">
+								<h6>Microsoft Excel 2010: Advanced Training</h6>
+								<div class="rating-wrapper">
+									<div class="rating-item">
+										<input type="hidden" class="rating" data-filled="fa fa-star" data-empty="fa fa-star-o" data-fractions="2" data-readonly value="3.5"/>
+									</div>
+									<span>(7 review)</span>
+								</div>
+								<span class="price">$199.<small>98</small></span>
+							</div>
+						</a>
+					</li>
+					<li class="clearfix">
+						<a href="#">
+							<div class="image">
+								<img src="images/course/course-item-sm-03.jpg" alt="Related Course" />
+							</div>
+							<div class="content">
+								<h6>The Java Spring Tutorial: Learn Java's Popular Web Framework</h6>
+								<div class="rating-wrapper">
+									<div class="rating-item">
+										<input type="hidden" class="rating" data-filled="fa fa-star" data-empty="fa fa-star-o" data-fractions="2" data-readonly value="3.5"/>
+									</div>
+									<span>(7 review)</span>
+								</div>
+								<span class="price">$199.<small>98</small></span>
+							</div>
+						</a>
+					</li>
+				</ul>
+
+			</div>
+
+		</div>
+	</div>
+
+	<div class="modal-footer text-center">
+		<button form="course-pur" type="button" class="btn btn-primary">خرید</button>
+		<button type="button" data-dismiss="modal" class="btn btn-dark">بستن</button>
+	</div>
 </div>
 
 <!-- JS -->
