4888b8f68b86ed9c5dddee49a18a83f03e1a5ca3
RshNk <rk.mirzaee.m@gmail.com>
2016-12-23 12:48:54 +0330

    final changes from roshanak

---
+++ resources/views/profile/credit-plus.blade.php
@@ -0,0 +1,385 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Title Of Site -->
+	<title>Edutute</title>
+	<meta name="description" content="HTML Responsive Landing Page Template for Course Online Based on Twitter Bootstrap 3.x.x" />
+	<meta name="keywords" content="study, learn, course, tutor, tutorial, teach, college, school, institute, teacher, instructor" />
+	<meta name="author" content="crenoveative">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<!-- Fav and Touch Icons -->
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+	<link rel="shortcut icon" href="images/ico/favicon.png">
+
+    <!-- CSS Plugins -->
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" media="screen">
+	<link rel="stylesheet" type="text/css" href="bootstrap-rtl-3.3.4/dist/css/bootstrap-rtl.min.css" media="screen">
+
+	<link href="css/animate.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/plugin.css" rel="stylesheet">
+
+	<!-- CSS Custom -->
+	<link href="css/style.css" rel="stylesheet">
+	
+	<!-- For your own style -->
+	<link href="css/your-style.css" rel="stylesheet">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	
+</head>
+
+<body id="credit">
+
+
+	<!-- start Container Wrapper -->
+	<div class="container-wrapper">
+
+		<!-- start Header -->
+		<header id="header">
+	  
+			@include('header')
+
+		</header>
+		<!-- end Header -->
+
+		<!-- start Main Wrapper -->
+		<div class="main-wrapper scrollspy-container">
+		
+			<div class="breadcrumb-wrapper">
+			
+				<div class="container">
+
+					<h1 class="page-title">افزایش اعتبار</h1>
+
+					<div class="row">
+
+						<div class="col-xs-12 col-sm-8">
+							<ol class="breadcrumb">
+								<li><a href="/">صفحه اصلی</a></li>
+								<li><a href="/profile">پروفایل</a></li>
+								<li class="active">افزایش اعتبار</li>
+							</ol>
+						</div>
+
+					</div>
+					
+				</div>
+
+			</div>
+			
+			<div class="equal-content-sidebar-wrapper detail-page-wrapper">
+				
+				<div class="equal-content-sidebar-by-gridLex">
+	
+					<div class="container">
+					
+						<div class="GridLex-grid-noGutter-equalHeight mr-40 ml-40">
+							
+							<div class="GridLex-col-12_sm-12_xs-12_xss-12">
+								
+								<div class="content-wrapper">
+							
+									<div class="payment-content-wrapper mb-0">
+
+										<form class="creditly-card-form">
+
+											<div class="row">
+											
+												<div class="col-xs-12 col-md-8">
+														<div class="section-title text-right mb-20">
+															<h3 class="mb-10">افزایش اعتبار</h3>
+														</div>
+												</div>
+												
+											</div>
+											
+											<div class="payment-content-box">
+											
+												<div class="GridLex-gap-30 no-mb featured-checkbox-wrapper no-bb-col-2">
+												
+													<div class="GridLex-grid-noGutter-equalHeight">
+						
+														<div class="GridLex-col-6_sm-6_xs-12_xss-12">
+														
+															<div class="featured-checkbox">
+																<div class="checkbox-block">
+																	<input id="featured_checkbox-1" name="featured_checkbox" type="checkbox" class="checkbox" checked/>
+																	<label class="clearfix" for="featured_checkbox-1">
+																		<span class="h6">افزایش ویژه</span>
+																		<span class="p">با 10% هدیه افزایش اعتبار</span>
+																		<span class="price">5 هزار تومان</span>
+																	</label>
+																</div>
+															</div>
+															
+														</div>
+														
+														<div class="GridLex-col-6_sm-6_xs-12_xss-12">
+														
+															<div class="featured-checkbox">
+																<div class="checkbox-block">
+																	<input id="featured_checkbox-2" name="featured_checkbox" type="checkbox" class="checkbox"/>
+																	<label class="clearfix" for="featured_checkbox-2">
+																		<span class="h6">افزایش ویژه</span>
+																		<span class="p">با 10% هدیه افزایش اعتبار</span>
+																		<span class="price">15 هزار تومان</span>
+																	</label>
+																</div>
+															</div>
+															
+														</div>
+														
+														<div class="GridLex-col-6_sm-6_xs-12_xss-12">
+														
+															<div class="featured-checkbox">
+																<div class="checkbox-block">
+																	<input id="featured_checkbox-3" name="featured_checkbox" type="checkbox" class="checkbox"/>
+																	<label class="clearfix" for="featured_checkbox-3">
+																		<span class="h6">افزایش ویژه</span>
+																		<span class="p">با 15% هدیه افزایش اعتبار</span>
+																		<span class="price">25 هزار تومان</span>
+																	</label>
+																</div>
+															</div>
+															
+														</div>
+														
+														<div class="GridLex-col-6_sm-6_xs-12_xss-12">
+														
+															<div class="featured-checkbox">
+																<div class="checkbox-block">
+																	<input id="featured_checkbox-4" name="featured_checkbox" type="checkbox" class="checkbox"/>
+																	<label class="clearfix" for="featured_checkbox-4">
+																		<span class="h6">مبالغ دیگر</span>
+																		<span class="p">در صورتی که از این گزینه استفاده کنید هدیه ی افزایش اعتبار به شما تعلق نمیگیرد.</span>
+																	</label>
+																</div>
+															</div>
+															
+														</div>
+														
+													</div>
+							
+												</div>
+
+												<div class="row" id="otherCost" hidden>
+
+													<div class="form-group">
+
+														<div class="col-lg-offset-6 col-xs-1 col-sm-6 col-md-1">
+
+															<label class="text-primary">مبلغ: </label>
+
+														</div>
+
+														<div class="col-xs-5 col-sm-12 col-md-5">
+
+															<input type="text" class="form-control" placeholder="10 هزار تومان" />
+
+														</div>
+													</div>
+												</div>
+
+												<div id="paymentPaypal" class="payment-option-form">
+													<div class="inner">
+
+														<h4 class="text-primary">مبلغ پرداختی: <span class="font700" id="current-charge">5 هزار تومان</span></h4>
+														<h4 class="text-success">مبلغ شارژ: <span class="font700" id="full-charge">5 هزار تومان</span></h4>
+
+													</div>
+
+												</div>
+
+											</div>
+
+											<div class="row">
+											
+												<div class="col-xs-12 col-md-8">
+														<div class="section-title text-left mb-20">
+															<h3 class="mb-10">انتخاب درگاه بانکی</h3>
+														</div>
+												</div>
+												
+											</div>
+											
+											<div class="payment-content-box">
+
+												<div id="paymentOption" class="payment-option-wrapper">
+	
+													<div class="row">
+													
+														<div class="col-sm-12">
+														
+															<div class="radio-block font-icon-radio">
+																<input id="payments1" name="payments" type="radio" class="radio" value="paymentsCreditCard"/>
+																<label class="" for="payments1"><span>بانک ملی</span> <img src="images/payment-credit-cards.jpg" alt="Image"></label>
+															</div>
+															
+														</div>
+														
+														<div class="clear mb-10"></div>
+														
+														<div class="col-sm-12">
+															<div class="radio-block font-icon-radio">
+																<input id="payments2" name="payments" type="radio" class="radio" value="paymentPaypal"/>
+																<label class="" for="payments2"><span>بانک تجارت</span> <img src="images/payment-paypal.jpg" alt="Image"></label>
+															</div>
+														</div>
+														
+													</div>
+												
+												</div>
+												
+											</div>
+									
+											<a href="#" class="btn btn-danger mt-10">پرداخت</a>
+											
+										</form>
+
+									</div>
+
+								</div>
+								
+							</div>
+
+						</div>
+						
+					</div>
+					
+				</div>
+
+			</div>
+			
+			
+		</div>
+		<!-- end Main Wrapper -->
+		
+		<!-- start Footer Wrapper -->
+		<div class="footer-wrapper scrollspy-footer">
+		
+			@include('footer')
+
+		</div>
+		<!-- end Footer Wrapper -->
+		
+	</div>
+	<!-- end Container Wrapper -->
+ 
+ 
+<!-- start Back To Top -->
+<div id="back-to-top">
+   <a href="#"><i class="ion-ios-arrow-up"></i></a>
+</div>
+<!-- end Back To Top -->
+
+<div id="ajaxLoginModal" class="modal fade login-box-wrapper" data-width="500" data-backdrop="static" data-keyboard="false" tabindex="-1" style="display: none;"></div>
+		
+<div id="ajaxRegisterModal" class="modal fade login-box-wrapper" data-width="500" data-backdrop="static" data-keyboard="false" tabindex="-1" style="display: none;">
+</div>
+
+<div id="ajaxForgotPasswordModal" class="modal fade login-box-wrapper" data-width="500" data-backdrop="static" data-keyboard="false" tabindex="-1" style="display: none;"></div>
+
+
+<!-- JS -->
+<script type="text/javascript" src="js/jquery-2.2.4.min.js"></script>
+<script type="text/javascript" src="js/jquery-migrate-1.4.1.min.js"></script>
+<script type="text/javascript" src="bootstrap-rtl-3.3.4/dist/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.waypoints.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/SmoothScroll.min.js"></script>
+<script type="text/javascript" src="js/spin.min.js"></script>
+<script type="text/javascript" src="js/jquery.introLoader.min.js"></script>
+<script type="text/javascript" src="js/typed.js"></script>
+<script type="text/javascript" src="js/placeholderTypewriter.js"></script>
+<script type="text/javascript" src="js/jquery.slicknav.min.js"></script>
+<script type="text/javascript" src="js/jquery.placeholder.min.js"></script>
+<script type="text/javascript" src="js/select2.full.js"></script>
+<script type="text/javascript" src="js/ion.rangeSlider.min.js"></script>
+<script type="text/javascript" src="js/readmore.min.js"></script>
+<script type="text/javascript" src="js/slick.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-rating.js"></script>
+<script type="text/javascript" src="js/jquery.nicescroll.min.js"></script>
+<script type="text/javascript" src="js/creditly.js"></script>
+<script type="text/javascript" src="js/bootstrap-modalmanager.js"></script>
+<script type="text/javascript" src="js/bootstrap-modal.js"></script>
+<script type="text/javascript" src="js/customs.js"></script>
+
+<script>
+!function ($) {
+
+  $(function(){
+
+    var $window = $(window)
+    var $body   = $(document.body)
+
+    var navHeight = $('.navbar').outerHeight(true) + 50
+
+    $body.scrollspy({
+      target: '.scrollspy-sidebar',
+      offset: navHeight
+    })
+
+    $window.on('load', function () {
+      $body.scrollspy('refresh')
+    })
+
+    $('.scrollspy-container [href=#]').click(function (e) {
+      e.preventDefault()
+    })
+
+    // back to top
+    setTimeout(function () {
+      var $sideBar = $('.scrollspy-sidebar')
+
+      $sideBar.affix({
+        offset: {
+          top: function () {
+            var offsetTop      = $sideBar.offset().top
+            var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
+            var navOuterHeight = $('.scrollspy-nav').height()
+
+            return (this.top = offsetTop - navOuterHeight - sideBarMargin)
+          }
+        , bottom: function () {
+            return (this.bottom = $('.scrollspy-footer').outerHeight(true))
+          }
+        }
+      })
+    }, 100)
+		
+  })
+
+}(window.jQuery)
+
+</script>
+
+
+<script type="text/javascript">
+$(function() {
+	var creditly = Creditly.initialize(
+		'.creditly-wrapper .expiration-month-and-year',
+		'.creditly-wrapper .credit-card-number',
+		'.creditly-wrapper .security-code',
+		'.creditly-wrapper .card-type');
+});
+</script>
+
+</body>
+
+</html>
